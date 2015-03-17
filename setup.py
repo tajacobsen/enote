d5a107e48d994daa2152f6f90d101dfc8d364844
Troels Kofoed Jacobsen <tkjacobsen@gmail.com>
2015-03-17 09:14:48 +0100

    Add executable script

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python2
 # Copyright (c) 2015 Troels Kofoed Jacobsen
+#from distutils.core import setup
 from distutils.core import setup
 
-setup(name='enote',
-      version='0.0',
-      description='Command line utility to backup Evernote notes and notebooks',
-      author='Troels Jacobsen',
-      author_email='tkjacobsen@gmail.com',
-      url='https://github.com/tkjacobsen/enote',
-      packages=['enote'],
-     )
+from enote import __version__
+
+setup(
+    name='enote',
+    version=__version__,
+    license='MIT',
+    description='Command line utility to backup Evernote notes and notebooks',
+    author='Troels Jacobsen',
+    author_email='tkjacobsen@gmail.com',
+    url='https://github.com/tkjacobsen/enote',
+    packages=['enote'],
+    scripts=['bin/enote'],
+    )
