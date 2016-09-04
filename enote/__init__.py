b3f60bcc34f2cd8b6acbfeae3a794a707d25f7fd
Troels Agergaard Jacobsen <tkjacobsen@gmail.com>
2016-09-04 20:39:59 +0200

    Strat from scratch

--- enote/__init__.py
+++ enote/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-# Copyright (c) 2015 Troels Kofoed Jacobsen
-__version__ = '0.0'
+# Copyright (c) 2016 Troels Agergaard Jacobsen
+__version__ = '0.2.0'
 __description__ = 'Command line utility to backup Evernote notes and notebooks.'
